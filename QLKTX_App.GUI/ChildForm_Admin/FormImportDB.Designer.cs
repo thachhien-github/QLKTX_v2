f6c177634f5e686b3788d9ec6b8bd22afae7fe60
tuhoc.cc <htbin2806@gmail.com>
2025-09-18 00:18:51 +0700

    xsa

--- QLKTX_App.GUI/ChildForm_Admin/FormImportDB.Designer.cs
+++ QLKTX_App.GUI/ChildForm_Admin/FormImportDB.Designer.cs
@@ -56,11 +56,11 @@
             this.btnImport.IconColor = System.Drawing.Color.LimeGreen;
             this.btnImport.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnImport.IconSize = 25;
-            this.btnImport.Location = new System.Drawing.Point(235, 108);
+            this.btnImport.Location = new System.Drawing.Point(241, 99);
             this.btnImport.MaximumSize = new System.Drawing.Size(275, 45);
             this.btnImport.Name = "btnImport";
             this.btnImport.Padding = new System.Windows.Forms.Padding(15, 3, 3, 3);
-            this.btnImport.Size = new System.Drawing.Size(107, 37);
+            this.btnImport.Size = new System.Drawing.Size(113, 42);
             this.btnImport.TabIndex = 3;
             this.btnImport.Text = "Import";
             this.btnImport.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -79,11 +79,11 @@
             this.btnBrowse.IconColor = System.Drawing.Color.LimeGreen;
             this.btnBrowse.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnBrowse.IconSize = 25;
-            this.btnBrowse.Location = new System.Drawing.Point(122, 108);
+            this.btnBrowse.Location = new System.Drawing.Point(122, 99);
             this.btnBrowse.MaximumSize = new System.Drawing.Size(275, 45);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Padding = new System.Windows.Forms.Padding(3);
-            this.btnBrowse.Size = new System.Drawing.Size(107, 37);
+            this.btnBrowse.Size = new System.Drawing.Size(113, 42);
             this.btnBrowse.TabIndex = 2;
             this.btnBrowse.Text = "Browse";
             this.btnBrowse.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -122,7 +122,7 @@
             this.dgvTable.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvTable.EnableHeadersVisualStyles = false;
             this.dgvTable.GridColor = System.Drawing.SystemColors.ControlLight;
-            this.dgvTable.Location = new System.Drawing.Point(10, 218);
+            this.dgvTable.Location = new System.Drawing.Point(10, 213);
             this.dgvTable.Margin = new System.Windows.Forms.Padding(10);
             this.dgvTable.MultiSelect = false;
             this.dgvTable.Name = "dgvTable";
@@ -145,7 +145,7 @@
             this.dgvTable.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.SystemColors.Control;
             this.dgvTable.RowTemplate.Height = 35;
             this.dgvTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvTable.Size = new System.Drawing.Size(1027, 355);
+            this.dgvTable.Size = new System.Drawing.Size(1027, 360);
             this.dgvTable.TabIndex = 3;
             // 
             // groupBox1
@@ -160,7 +160,7 @@
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(10, 60);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1027, 158);
+            this.groupBox1.Size = new System.Drawing.Size(1027, 153);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             // 
@@ -169,7 +169,7 @@
             this.txtFilePath.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtFilePath.Location = new System.Drawing.Point(122, 63);
             this.txtFilePath.Name = "txtFilePath";
-            this.txtFilePath.Size = new System.Drawing.Size(312, 30);
+            this.txtFilePath.Size = new System.Drawing.Size(232, 30);
             this.txtFilePath.TabIndex = 1;
             // 
             // cboTable
@@ -178,14 +178,14 @@
             this.cboTable.FormattingEnabled = true;
             this.cboTable.Location = new System.Drawing.Point(122, 26);
             this.cboTable.Name = "cboTable";
-            this.cboTable.Size = new System.Drawing.Size(312, 31);
+            this.cboTable.Size = new System.Drawing.Size(232, 31);
             this.cboTable.TabIndex = 0;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(17, 66);
+            this.label3.Location = new System.Drawing.Point(17, 63);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 23);
             this.label3.TabIndex = 7;
