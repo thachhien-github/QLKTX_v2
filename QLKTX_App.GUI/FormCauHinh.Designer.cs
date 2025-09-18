98d6cc6de93bc633b888bfd3af573f2df0aec3bd
tuhoc.cc <htbin2806@gmail.com>
2025-09-18 13:56:53 +0700

    fff

--- QLKTX_App.GUI/FormCauHinh.Designer.cs
+++ QLKTX_App.GUI/FormCauHinh.Designer.cs
@@ -173,7 +173,9 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(551, 381);
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(551, 381);
             this.Name = "FormCauHinh";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cấu hình Server";
